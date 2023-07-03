6c180e357b039d6c0edd78005beab9999cb087ef
funnoodle11 <81544214+funnoodle11@users.noreply.github.com>
2023-07-03 13:38:12 -0700

    Create Super-Permutation.cpp

---
+++ Super-Permutation.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n==1) cout<<1<<endl;
+        else if (n%2==1) cout<<-1<<endl;
+        else{
+            for(int i=0;i<n;i++){
+                if(i%2==0) cout<<n-i<<' ';
+                else cout<<i<<' ';
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+    
+}
